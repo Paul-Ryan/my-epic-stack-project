6b0a36fecd495b7d9ecd98f52a7125b5146a18ed
Paul Ryan <paul.ryan@quantcast.com>
2024-10-24 16:49:30 -0700

    Add default colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,26 @@ import animatePlugin from 'tailwindcss-animate'
 import radixPlugin from 'tailwindcss-radix'
 import { marketingPreset } from './app/routes/_marketing+/tailwind-preset'
 import { extendedTheme } from './app/utils/extended-theme.ts'
+const colors = require('tailwindcss/colors')
 
 export default {
 	content: ['./app/**/*.{ts,tsx,jsx,js}'],
 	darkMode: 'class',
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.slate[900],
+			white: colors.white,
+			gray: {
+				DEFAULT: colors.gray[800],
+				light: colors.gray[300],
+				dark: colors.gray[900],
+			},
+			emerald: colors.emerald,
+			indigo: colors.indigo,
+			yellow: colors.yellow,
+		},
 		container: {
 			center: true,
 			padding: '2rem',
